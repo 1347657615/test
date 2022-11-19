6cd5b62f41db09aa4f40d7a5b4a33a14dd3f53ef
1347657615 <1347657615@QQ.com>
2022-11-19 23:51:32 +0800

    branch main commit Hello.py

--- Hello.py
+++ Hello.py
@@ -1,2 +1,16 @@
 # -*- coding:utf-8 -*-
 print('Hello world!')
+
+def hello():
+    print('Hello World!')
+
+
+def main():
+    inputbox = input('Please write:')
+    if inputbox == 'hello':
+        return hello()
+
+
+if __name__=='__main__':
+    main()
+    
